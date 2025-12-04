5241169643d6ffa24b99477da8c642cfb9152eae
Callum Tilbury <callum@tilbury.co.za>
2025-12-04 14:16:39 +0200

    speedy jit demo!

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import jax
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+from chex import assert_max_traces
 
 from jbubble import (
     Units,
+    SaveSpec,
     compute_radius_metrics,
     default_bubble,
     default_pulse,
@@ -13,10 +17,34 @@ from jbubble import (
 def demo_run(R0=4e-6, pressure=500e3, freq=800e3):
     units = Units()
     bubbleA = default_bubble(R0=R0)
-    bubbleB = default_bubble(R0=R0 * 1.5)
-    pulse = default_pulse(freq=freq, pressure=pressure)
-    resultA = run_simulation(bubble=bubbleA, pulse=pulse, units=units)
-    resultB = run_simulation(bubble=bubbleB, pulse=pulse, units=units)
+    # bubbleB = default_bubble(R0=R0)
+
+    pulseA = default_pulse(freq=freq, pressure=pressure)
+    pulseB = default_pulse(freq=freq * 1.1, pressure=pressure * 1.1)
+    pulseC = default_pulse(freq=freq * 1.1, pressure=pressure * 1.1)
+
+    save_spec = SaveSpec(num_samples=1000)
+
+    jit_time = time.perf_counter()
+    jit_sim = jax.jit(assert_max_traces(run_simulation, n=1))
+    end_jit_time = time.perf_counter()
+    print(f"JIT compilation took {end_jit_time - jit_time:.4f} seconds")
+
+    start_A = time.perf_counter()
+    resultA = jit_sim(bubble=bubbleA, pulse=pulseA, units=units, save_spec=save_spec)
+    end_A = time.perf_counter()
+
+    start_B = time.perf_counter()
+    resultB = jit_sim(bubble=bubbleA, pulse=pulseB, units=units, save_spec=save_spec)
+    end_B = time.perf_counter()
+
+    start_C = time.perf_counter()
+    resultC = jit_sim(bubble=bubbleA, pulse=pulseC, units=units, save_spec=save_spec)
+    end_C = time.perf_counter()
+
+    print(f"Simulation A took {end_A - start_A:.4f} seconds")
+    print(f"Simulation B took {end_B - start_B:.4f} seconds")
+    print(f"Simulation C took {end_C - start_C:.4f} seconds")
     metricsA = compute_radius_metrics(resultA)
     metricsB = compute_radius_metrics(resultB)
 
