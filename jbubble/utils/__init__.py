b35445e129c642cedb44cd49d65ddec5efe9b00c
Callum Rhys Tilbury <callum@tilbury.co.za>
2026-03-18 12:17:53 +0000

    feat: add quick-start simulation presets for microbubble models

--- jbubble/utils/__init__.py
+++ jbubble/utils/__init__.py
@@ -1,10 +1,7 @@
 """jbubble.utils — small general-purpose utilities."""
 
 from .gridsweep import GridSweep
-from .io import export_hdf5, load_hdf5
 
 __all__ = [
     "GridSweep",
-    "export_hdf5",
-    "load_hdf5",
 ]
